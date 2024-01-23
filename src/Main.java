b578bd36a38e217ba0ca968161d6e51c72597756
Jorge Sacasa <jasacasa@loyola.edu>
2024-01-23 13:52:57 -0500

    submission

---
+++ src/Main.java
@@ -0,0 +1,25 @@
+// Programmers:  Jorge Sacasa
+// Course:  CS 212, Professor Nweke
+// Due Date: 1/19/24
+// Lab Assignment: 0
+// Problem Statement:  Output programmers’ initial
+// Input: None; there is no user input in this program
+// Output:  The initials of the programmers in ASCII art form
+// Credits: [Is your code based on an example in the book, in class, or something else?
+//            Reminder: you should never take code from the Internet or another person
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.printf("Hello world!\n\n");
+        System.out.printf("  *********\n");
+        System.out.printf("      *\n");
+        System.out.printf("      *\n");
+        System.out.printf("      *\n");
+        System.out.printf("   *  *\n");
+        System.out.printf("   ****\n");
+
+
+
+
+    }
+}
